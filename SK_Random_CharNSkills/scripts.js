7907d0b864ac095b7e408d5bc5f0ff0b782ec60e
COwOkies_ <54218300+WolfyKiro@users.noreply.github.com>
2022-07-13 23:14:05 +0200

    Add files via upload

--- SK_Random_CharNSkills/scripts.js
+++ SK_Random_CharNSkills/scripts.js
@@ -1,13 +1,14 @@
 let characters = ['Knight', 'Rogue', 'Wizard', 'Assassin', 'Alchemist', 'Engineer', 'Vampire', 'Paladin', 'Elf', 'Werewolf', 'Priest', 'Druid', 'Robot', 'Berseker', 'Necromancer', 'Officer', 'Taoist', 'Air Bender', 'Demonmancer'];
 var randomChar = -1
 var randomSkill = -1
+var color = 1
 
 function showRandomChar(){
     old_randomChar = randomChar
     randomChar = rando(0, 18);
     while (randomChar == old_randomChar) {
         randomChar = rando(0, 18);}
-    if (randomChar > 17) {
+    if (randomChar > 16) {
         randomSkill = 0
         document.getElementById("mySkill").innerHTML = (randomSkill+1);}
     document.getElementById("myCharacter").innerHTML = characters[randomChar];
@@ -18,7 +19,7 @@ function showRandomChar(){
 
 function showRandomSkill(){
     old_randomSkill = randomSkill
-    if (randomChar > 17) {randomSkill = 0}
+    if (randomChar > 16) {randomSkill = 0}
     else {randomSkill = rando(0, 2);
         while (randomSkill == old_randomSkill) {
             randomSkill = rando(0, 2); }     
@@ -34,7 +35,20 @@ function setCharimage(){
     image.src = image_path;}
 
 function setSkillimage(){
-
     const image_path2 = ("skills/"+randomChar+"-"+randomSkill+".png");
     const image = document.getElementById("Char_Skill");
     image.src = image_path2;}
+
+function switchMode() {
+    color = color + 1
+    if (color > 1) {color = 0;}
+    changeColor()
+}
+
+function changeColor() {
+    var element = document.body;
+    if (color == 1) {element.className = "dark-mode";
+    document.getElementById("color").innerHTML = "☀️";}
+    if (color == 0) {element.className = "light-mode";
+    document.getElementById("color").innerHTML = "🌙";}
+}
