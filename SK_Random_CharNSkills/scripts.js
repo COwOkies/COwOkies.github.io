35df6254ed2f08a3f61a8c4c9292b3c953c04b8c
COwOkies_ <54218300+WolfyKiro@users.noreply.github.com>
2022-07-15 12:12:57 +0200

    Add files via upload

--- SK_Random_CharNSkills/scripts.js
+++ SK_Random_CharNSkills/scripts.js
@@ -1,11 +1,14 @@
 let characters = ['Knight', 'Rogue', 'Wizard', 'Assassin', 'Alchemist', 'Engineer', 'Vampire', 'Paladin', 'Elf', 'Werewolf', 'Priest', 'Druid', 'Robot', 'Berseker', 'Necromancer', 'Officer', 'Taoist', 'Air Bender', 'Demonmancer', 'Inter-dimension Traveler', 'Element Envoy', 'The Beheaded', 'Time Traveling Ninja','Special Forces'];
-let charprice = ['Free Char','f2p (2000)','f2p (3000)','f2p (4000)','f2p (5000)','Paid ($0.99)','Paid ($0.99)','Paid ($0.99)','f2p (12000)','Paid ($1.99)','f2p (12000)','Paid ($1.99)','f2p (IG Materials)','Paid ($1.99)','Paid ($1.99)','f2p (achievement)','Paid ($1.99)','f2p (achievement)','Paid ($1.99)','Paid (Priest+Necromancer) ','f2p (Wizard+Elf)','f2p (IG Materials)','Paid (Assassin+Engineer)','f2p (Officer+Knight)'];
+let charprice = ['Free Char','f2p (2000)','f2p (3000)','f2p (4000)','f2p (5000)','Paid ($0.99)','Paid ($0.99)','Paid ($0.99)','f2p (12000)','Paid ($1.99)','f2p (12000)','Paid ($1.99)','f2p (IG Materials)','Paid ($1.99)','Paid ($1.99)','f2p (achievement)','Paid ($1.99)','f2p (achievement)','Paid ($1.99)','Paid (Priest+Necromancer)','f2p (Wizard+Elf)','f2p (IG Materials)','Paid (Assassin+Engineer)','f2p (Officer+Knight)'];
 let skillprice =[['Free Skill','f2p (5000)','f2p (6000)'],['Free Skill','Paid ($1.99)','Paid ($1.99)'],['Free Skill','f2p (6000)','Paid ($1.99)'],['Free Skill','f2p (7000)','f2p (7000)'],['Free Skill','f2p (8000)','Paid ($1.99)'],['Free Skill','Paid ($1.99)','Paid ($1.99)'],['Free Skill','Paid ($1.99)','f2p (10000)'],['Free Skill','Paid ($1.99)','f2p (Design Table)'],['Free Skill','Paid ($1.99)','f2p (10000)'],['Free Skill','f2p (12000)','Paid ($1.99)'],['Free Skill','f2p (Design Table)','Paid ($1.99)'],['Free Skill','f2p (Design Table)','f2p (10000)'],['Free Skill','Paid ($1.99)','f2p (8000)'],['Free Skill','Paid ($1.99)','f2p (Design Table)'],['Free Skill','Paid ($1.99)','Paid ($1.99)'],['Free Skill','f2p (8000)','Paid ($1.99)'],['Free Skill','Paid ($1.99)','f2p (10000)'],['Free Skill'],['Free Skill'],['Free Skill'],['Free Skill'],['Free Skill'],['Free Skill'],['Free Skill']]
+let charmode = [0,0,0,0,0,1,1,1,0,1,0,1,0,1,1,0,1,0,1,1,0,0,1,0];
+let skillmode =[[0,0,0],[0,1,1],[0,0,1],[0,0,0],[0,0,1],[0,1,1],[0,1,0],[0,1,0],[0,1,0],[0,0,1],[0,0,1],[0,0,0],[0,1,0],[0,1,0],[0,1,1],[0,0,1],[0,1,0],[0],[0],[0],[0],[0],[0],[0]]
 var randomChar = -1
 var randomSkill = -1
 var color = 1
 var minvalue = 0
 var maxvalue = 18
+var f2p = false
 var path = ("skins/"+randomChar+".png")
 
 
@@ -16,18 +19,35 @@ function showRandomChar(){
     if (randomChar > 16) {
         randomSkill = 0
         document.getElementById("buttontohide").style.color = 'gray';
-        document.getElementById("buttontohide").style.borderBlockColor = 'gray';
-        document.getElementById("mySkill").innerHTML = (randomSkill+1);}
+        document.getElementById("buttontohide").style.borderBlockColor = 'gray';}
     else {document.getElementById("buttontohide").style.color = 'white';
     document.getElementById("buttontohide").style.borderBlockColor = 'white';}
+    if (f2p == true) {
+        while (skillmode[randomChar][randomSkill] == 1) {randomSkill = rando(0, 2);}
+        while (charmode[randomChar] == 1) {randomChar = rando(minvalue, maxvalue);}}
     if (randomChar == 23) {path = ("skins/23.gif")}
     else {path = ("skins/"+randomChar+".png")}
     document.getElementById("myCharacter").innerHTML = characters[randomChar];
     document.getElementById("price").innerHTML = charprice[randomChar];
+    document.getElementById("mySkill").innerHTML = (randomSkill+1);
     document.getElementById("priceskill").innerHTML = skillprice[randomChar][randomSkill];
 
     setCharimage()
     setSkillimage()
+}
+
+function showRandomSkill(){
+    old_randomSkill = randomSkill
+    if (randomChar > 16) {randomSkill = 0}
+    else {randomSkill = rando(0, 2);
+        while (randomSkill == old_randomSkill) {
+            randomSkill = rando(0, 2); }     
+    }
+    if (f2p == true) {
+        while (skillmode[randomChar][randomSkill] == 1) {randomSkill = rando(0, 2); }}
+    document.getElementById("mySkill").innerHTML = (randomSkill+1);
+    setSkillimage()
+    document.getElementById("priceskill").innerHTML = skillprice[randomChar][randomSkill];
 }
 
 
@@ -76,26 +96,26 @@ function onlylegend() {
     if (document.querySelector('#legendary4').checked == false) {
         minvalue = 0;
     }
-    }
+}
 
-    function prices() {
-        if (document.querySelector('#price2').checked == true) {
-            document.querySelector('#price').style.visibility = "visible";
-            document.querySelector('#priceskill').style.visibility = "visible";
-        }
-        if (document.querySelector('#price2').checked == false) {
-            document.querySelector('#price').style.visibility = "hidden";
-            document.querySelector('#priceskill').style.visibility = "hidden";
-        }}
-
-function showRandomSkill(){
-    old_randomSkill = randomSkill
-    if (randomChar > 16) {randomSkill = 0}
-    else {randomSkill = rando(0, 2);
-        while (randomSkill == old_randomSkill) {
-            randomSkill = rando(0, 2); }     
+function prices() {
+    if (document.querySelector('#price2').checked == true) {
+        document.querySelector('#price').style.visibility = "visible";
+        document.querySelector('#priceskill').style.visibility = "visible";
     }
-    document.getElementById("mySkill").innerHTML = (randomSkill+1);
-    setSkillimage()
-    document.getElementById("priceskill").innerHTML = skillprice[randomChar][randomSkill];
+    if (document.querySelector('#price2').checked == false) {
+        document.querySelector('#price').style.visibility = "hidden";
+        document.querySelector('#priceskill').style.visibility = "hidden";
+    }
+}
+
+function enablef2p() {
+    if (document.querySelector('#f2p2').checked == true) {
+        f2p = true
+        showRandomSkill()
+        showRandomChar()
+    }
+    if (document.querySelector('#f2p2').checked == false) {
+        f2p = false
+    }
 }
