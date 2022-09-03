f05788fd61a249dea0d90f34c4613b6cb87cdb62
COwOkies_ <54218300+COwOkies@users.noreply.github.com>
2022-09-04 00:07:48 +0200

    Add files via upload

--- websiteomgwhathaveidone/code.js
+++ websiteomgwhathaveidone/code.js
@@ -2,7 +2,11 @@ function over() {
   document.getElementById("pfp").src = "discord.png";
 }
 function clicked() {
-  window.prompt("Copy to clipboard (Ctrl+C) and then close this window", "COwOkie#6114")
+  //window.prompt("Copy to clipboard (Ctrl+C) and then close this window", "COwOkie#6114")
+  navigator.clipboard.writeText("COwOkie#6114");
+  
+  /* Alert the copied text */
+  alert("COwOkies discord copied to clipboard");
 }
 function noover() {
   document.getElementById("pfp").src = "cookie.png";}
