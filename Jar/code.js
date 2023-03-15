af9e518c3b694af054205e94d0207974c7bc3749
COwOkies_ <54218300+COwOkies@users.noreply.github.com>
2023-03-15 20:09:48 +0100

    Add files via upload

---
+++ Jar/code.js
@@ -0,0 +1,23 @@
+function clicked() {
+  navigator.clipboard.writeText("COwOkie#6114");
+  alert("COwOkies discord copied to clipboard");
+}
+
+function over() {
+  document.getElementById("pfp").src = "discord.png";
+}
+
+function noover() {
+  document.getElementById("pfp").src = "pdp.png";}
+
+function loadHeader() {
+  fetch("header.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("#bar").innerHTML = data;
+    });
+}
+
+window.addEventListener("DOMContentLoaded", loadHeader);
